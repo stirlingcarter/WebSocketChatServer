e81d2868e02474098195101545236498352b3a5a
stirlingcarter <stirlingcarter@gmail.com>
2024-10-10 17:12:22 -0700

    startup script and timings

--- start_script.sh
+++ start_script.sh
@@ -17,4 +17,14 @@ fi
 # Continue with the rest of your startup script
 source tub-env/bin/activate
 cd tubtitles/app
-python cuke.py
+# Find the latest version of the ICHY script
+latest_file=$(ls ICHY-*.py 2>/dev/null | sort -V | tail -n 1)
+
+# Check if the latest_file variable is not empty
+if [[ -n "$latest_file" ]]; then
+    echo "Running the latest script: $latest_file"
+    python "$latest_file"
+else
+    echo "No ICHY script found."
+    exit 1
+fi
